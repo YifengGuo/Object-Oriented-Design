9e01e9deef40998a43f95aa2d6eb90d593c446f7
YifengGuo <gyf940517@gmail.com>
2018-03-28 13:55:28 -0400

    Request and InvalidRequestException

---
+++ src/elevator/main/InvalidRequestException.java
@@ -0,0 +1,10 @@
+package elevator.main;
+
+/**
+ * Created by guoyifeng on 3/28/18.
+ */
+public class InvalidRequestException extends Exception{
+    public InvalidRequestException(String s) {
+        super(s);
+    }
+}
