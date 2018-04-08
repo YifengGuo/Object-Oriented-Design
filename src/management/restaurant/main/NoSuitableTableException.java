a1aefc858e75b1fbe6f2d56414c3afa936bd113b
YifengGuo <gyf940517@gmail.com>
2018-04-08 16:26:41 -0400

    used when no suitable table can be found for current party

---
+++ src/management/restaurant/main/NoSuitableTableException.java
@@ -0,0 +1,10 @@
+package management.restaurant.main;
+
+/**
+ * Created by guoyifeng on 4/8/18.
+ */
+public class NoSuitableTableException extends Exception {
+    public NoSuitableTableException(String msg) {
+        super(msg);
+    }
+}
