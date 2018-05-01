12a946e13514cb804b61b7989088d6cbe95df70c
YifengGuo <gyf940517@gmail.com>
2018-05-01 13:55:20 -0400

    exception when no proper book reader can be created by factory

---
+++ src/entity/main/Kindle/BookReaderException.java
@@ -0,0 +1,10 @@
+package entity.main.Kindle;
+
+/**
+ * Created by guoyifeng on 5/1/18.
+ */
+public class BookReaderException extends Exception {
+    public BookReaderException(String msg) {
+        super(msg);
+    }
+}
