738647b16bbe00470156c296cf6ac3b6d99319e3
YifengGuo <gyf940517@gmail.com>
2018-05-04 16:10:18 -0400

    handle max player exception

---
+++ src/game/main/black_jack/MaxPlayerNumberException.java
@@ -0,0 +1,10 @@
+package game.main.black_jack;
+
+/**
+ * Created by guoyifeng on 5/4/18.
+ */
+public class MaxPlayerNumberException extends Exception {
+    public MaxPlayerNumberException(String msg) {
+        super(msg);
+    }
+}
